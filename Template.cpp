7fb02b698b185756a2961a6ced9ae0a7b09abddd
elasree <130309298+elasree@users.noreply.github.com>
2023-08-02 22:37:10 +0530

    Initial commit

---
+++ Template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+void solve(void)
+{
+    cout << "HELLO" << endl;
+}
+signed main(void)
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
+    int _ = 1;
+    // cin >> _;
+    while (_--)
+        solve();
+    return 0;
+}
